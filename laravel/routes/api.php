853b38f8edea2f0981f583b1e3b5f60f6323b4a7
Rogeliotoro <rogeltoro@gmail.com>
2022-05-05 11:47:46 +0200

    added auth jwt, endpoint registrer

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ContactController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,6 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::group([
+    'middleware' => 'jwt.auth'
+], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/profile', [AuthController::class, 'me']);
+});
+//contacts
 Route::get('/contacts', [ContactController::class, 'getAllContacts']);
 Route::get('/contacts/{id}', [ContactController::class, 'getContactById']);
 Route::post('/contacts', [ContactController::class, 'createContact']);
