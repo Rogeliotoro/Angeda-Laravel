a99683ae4a1f3ca55a605a078246ff9b9e920e01
Rogeliotoro <rogeltoro@gmail.com>
2022-05-05 12:31:17 +0200

    added login with auth jws

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        try{
+        try {
             $validator = Validator::make($request->all(), [
                 'name' => 'required|string|max:255',
                 'email' => 'required|string|email|max:255|unique:users',
@@ -27,8 +28,29 @@ class AuthController extends Controller
             ]);
             $token = JWTAuth::fromUser($user);
             return response()->json(compact('user', 'token'), 201);
-        }catch(\Throwable $th){
-            return response()->json(['error => "error registering user'] ,500);
+        } catch (\Throwable $th) {
+            return response()->json(['error => "error registering user'], 500);
         }
+    }
+    public function login(Request $request)
+    {
+        try{
+            $input = $request->only('email', 'password');
+        $jwt_token = null;
+
+        if (!$jwt_token = JWTAuth::attempt($input)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid Email or Password',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return response()->json([
+            'success' => true,
+            'token' => $jwt_token,
+        ]);
+        }catch(\Throwable $th) {
+            return response()->json(['error => "Error login user'], 500);
+        } 
     }
 }
